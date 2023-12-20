9ed5e4fc8548d35ac33049106922c4f356afce90
neverlane <naer200469@gmail.com>
2023-12-20 21:12:06 +0300

    added blast.hk topic link

--- client/src/components/sidebar/sidebar.tsx
+++ client/src/components/sidebar/sidebar.tsx
@@ -1,4 +1,4 @@
-import { Flex, Space, useComputedColorScheme } from '@mantine/core';
+import { Flex, Space, useComputedColorScheme, Text } from '@mantine/core';
 import { IconDeviceVisionPro } from '@tabler/icons-react';
 import { ColorScheme } from './color-scheme';
 import { Controls } from './controls';
@@ -8,8 +8,17 @@ export const Sidebar = () => {
   const colorScheme = useComputedColorScheme('dark');
   return (
     <Flex w={'20rem'} h={'100%'} direction={'column'} align={'start'} justify={'space-between'} p={'md'}>
-      <Flex w={'100%'} direction={'column'} align={'start'} justify={'start'}>
-        <IconDeviceVisionPro style={{ margin: '0 auto' }} size={'8rem'} color={colorScheme === 'dark' ? 'white' : 'black'} />
+      <Flex w={'100%'} direction={'column'} align={'center'} justify={'start'}>
+        <IconDeviceVisionPro size={'8rem'} color={colorScheme === 'dark' ? 'white' : 'black'} />
+        <Text size={'1.75rem'} fw={'bold'}>
+          GlobeVision
+        </Text>
+        <Text size={'1.25rem'}>map</Text>
+        <Space h={'xs'} />
+        <a href={'https://www.blast.hk/threads/196970/'} target={'_blank'} style={{ textDecoration: 'none' }}>
+          <Text fw={'bold'} size={'1rem'} >see topic on blast.hk</Text>
+        </a>
+        <Space h={'xl'} />
         <Controls />
         <Space h={'md'} />
         <PlayersList />
