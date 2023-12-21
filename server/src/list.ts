48322211d72cde7b9891072b8ac57e4e4bcb2c57
neverlane <naer200469@gmail.com>
2023-12-21 21:26:04 +0300

    update list.json and added map_type

--- server/src/list.ts
+++ server/src/list.ts
@@ -2,7 +2,14 @@ import { readFileSync } from 'fs';
 
 export interface IServerListItem {
   name: string;
+  mapType: string;
   index: number;
 }
 
-export const serversList = <IServerListItem[]> JSON.parse(readFileSync('list.json').toString());
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+export const serversList = <IServerListItem[]> JSON.parse(readFileSync('list.json').toString()).map(({name, map_type}, index) => ({
+  name,
+  mapType: map_type,
+  index
+}));
